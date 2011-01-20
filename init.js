bcfa9d8bfcce7e5aa703eb0a309871bce312a160
Semka Novikov <me@sdfgh153.ru>
2011-01-20 23:10:49 +0500

    Fits iteration, it already works.

---
+++ init.js
@@ -0,0 +1,27 @@
+var badgeCount = 0;
+setInterval("checkServerStatus()", 5000);
+
+
+function checkServerStatus() 
+{
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://www.dragongoserver.net/rss/status.php", true);
+    request.onreadystatechange = function() {
+	if (request.readyState == 4) {
+	    countGames(request.responseXML.getElementsByTagName('item'));
+	}
+    }
+    request.send();
+    badgeCount++;
+}
+
+
+function countGames(rssItems) 
+{
+    console.log(rssItems.item(0).childNodes[1]);
+    if (rssItems.length == 1 && rssItems.item(0).childNodes[1].firstChild.nodeValue == 'Empty lists') {
+	chrome.browserAction.setBadgeText({'text': ''});
+    } else {
+	chrome.browserAction.setBadgeText({'text': rssItems.length.toString()});
+    }
+}
