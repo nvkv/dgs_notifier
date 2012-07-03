da73560c766d255874411a76ce472835782eff10
Sёmka Novikov <semka.novikov@gmail.com>
2012-07-03 23:16:46 +0400

    - Fixed issue with DGS flood protection - Moved to new manifest version

--- init.js
+++ init.js
@@ -1,54 +1,63 @@
-var statusUrl = 'http://www.dragongoserver.net/status.php';
-var rssUrl    = 'http://www.dragongoserver.net/rss/status.php';
+var statusUrl      = 'http://www.dragongoserver.net/status.php';
+var quickStatusUrl = 'http://www.dragongoserver.net/quick_status.php'
 
 function checkServerStatus() {
-    var request = new XMLHttpRequest();
-    request.open("GET", rssUrl, true);
-    request.onreadystatechange = function() {
-	if (request.readyState == 4) {
-	    countGames(request.responseXML.getElementsByTagName('item'));
-	}
-    }
-    request.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 1984 00:00:00 GMT");
-    request.send();
+  var request = new XMLHttpRequest();
+  request.open("GET", quickStatusUrl, true);
+  request.onreadystatechange = function() {
+   if (request.readyState == 4) {    
+    countGamesInQuickStatus(request.responseText)
+   }
+ }
+ request.setRequestHeader("If-Modified-Since", "Sat, 1 Jan 1984 00:00:00 GMT");
+ request.send();
 }
 
 
-function countGames(rssItems) {
-    if (rssItems.length == 1 && rssItems.item(0).childNodes[1].firstChild.nodeValue == 'Empty lists') {
-	chrome.browserAction.setBadgeText({'text': ''});
-    } else {
-	chrome.browserAction.setBadgeText({'text': rssItems.length.toString()});
-    }
+function countGamesInQuickStatus(quickStatusText) {  
+  var count = 0;
+  var pattern = "'G'"
+  var regex = new RegExp(pattern,'gi');  
+  
+  while (regex.exec(quickStatusText)){    
+    count++;
+  }
+
+  if (count > 0) {
+    chrome.browserAction.setBadgeText({'text': count.toString()});
+  }
+  else {
+    chrome.browserAction.setBadgeText({'text': ''});
+  }
 }
 
 
 function findOrOpenDGSStatusTab() {
-    chrome.tabs.getAllInWindow(null, function (tabs) {
-	var dgsTab = null;
-	for (var i = 0; i < tabs.length; i++) {
-	    var tab = tabs[i];
-	    if (tab.url == statusUrl) {
-		dgsTab = tab;
-	    }
-	}
-	openDGSTab(dgsTab);
-    });
+  chrome.tabs.getAllInWindow(null, function (tabs) {
+   var dgsTab = null;
+   for (var i = 0; i < tabs.length; i++) {
+     var tab = tabs[i];
+     if (tab.url == statusUrl) {
+      dgsTab = tab;
+    }
+  }
+  openDGSTab(dgsTab);
+});
 }
 
 
 function openDGSTab(dgsTab) {
-    if (dgsTab == null) {
-	chrome.tabs.create({'url': statusUrl}); 
-    } else {
-	chrome.tabs.update(dgsTab.id, {'selected' : true, 'url' : statusUrl}, function(){})
-    }
+  if (dgsTab == null) {
+   chrome.tabs.create({'url': statusUrl}); 
+ } else {
+   chrome.tabs.update(dgsTab.id, {'selected' : true, 'url' : statusUrl}, function(){})
+ }
 }
 
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    findOrOpenDGSStatusTab();
+chrome.browserAction.onClicked.addListener(function(tab) {  
+  findOrOpenDGSStatusTab();
 });
 
 
-setInterval(checkServerStatus, 5000);
+setInterval(checkServerStatus, 60000);
