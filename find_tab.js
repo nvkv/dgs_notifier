01f27189b430b75ebf800afe644d1edc5b8da5e3
Sёmka Novikov <semka.novikov@gmail.com>
2012-11-30 22:44:38 +0600

    And tab magic again

--- find_tab.js
+++ find_tab.js
@@ -1,26 +1,26 @@
 function findOrOpenDGSStatusTab() 
 {
-  chrome.tabs.getAllInWindow(null, function (tabs) {
-    var dgsTab = null;
-    for (var i = 0; i < tabs.length; i++) {
-      var tab = tabs[i];
-      if (tab.url == statusUrl) {
-        dgsTab = tab;
-      }
-    }
-    openDGSTab(dgsTab);
-  });
+	chrome.tabs.getAllInWindow(null, function (tabs) {
+		var dgsTab = null;
+		for (var i = 0; i < tabs.length; i++) {
+			var tab = tabs[i];
+			if (tab.url == statusUrl) {
+				dgsTab = tab;
+			}
+		}
+		openDGSTab(dgsTab);
+	});
 }
 
 
 function openDGSTab(dgsTab) 
 {
 	chrome.browserAction.setBadgeText({'text': '?'});
-  	if (dgsTab == null) {
- 	  chrome.tabs.create({'url': statusUrl});
- 	 } 
- 	 else {
-  	  chrome.tabs.update(dgsTab.id, {'selected' : true, 'url' : statusUrl}, function(){})
- 	 }
+	if (dgsTab == null) {
+ 		chrome.tabs.create({'url': statusUrl});
+ 	} 
+ 	else {
+		chrome.tabs.update(dgsTab.id, {'selected' : true, 'url' : statusUrl}, function(){})
+ 	}
 }
 
