c2d41c79b6780aeb202b9fc7f5aebeaf36b67daa
Sёmka Novikov <semka.novikov@gmail.com>
2012-12-01 12:18:57 +0600

    Tabs magic again

--- background_task.js
+++ background_task.js
@@ -23,25 +23,25 @@ function startChecking()
 	var options = null
 
 	if (localStorage["background_task_id"] != null) {
-    	clearInterval(localStorage["background_task_id"])
+		clearInterval(localStorage["background_task_id"])
   	}
   
 	if (localStorage['dgs_options']) {
- 		 options = JSON.parse(localStorage['dgs_options'])
+		options = JSON.parse(localStorage['dgs_options'])
 	}
 
  	if (options == null) {
- 		options = {}
+		options = {}
  	}
 
   	var freq = options.frequency
   	if (freq == null) {
-    	freq = 300000;
+		freq = 300000;
   	}
-  
- 	 var intervalId = setInterval(refreshGames, freq);  
- 	 localStorage["background_task_id"] = intervalId;
- 	 refreshGames()
+
+	var intervalId = setInterval(refreshGames, freq);  
+	localStorage["background_task_id"] = intervalId;
+	refreshGames()
 }
 
 startChecking()
